e39029912979371e45d57195ca76e4c1f90e905b
QizzyCO <terapptq@gmail.com>
2026-02-04 20:41:56 +0700

    Add galleryImageUrls to galleryUrls.js

---
+++ Web-Kelas-V2-main/src/data/galleryUrls.js
@@ -0,0 +1,5 @@
+export const galleryImageUrls = [
++	"https://drive.google.com/uc?export=view&id=FILE_ID_1",
++	"https://drive.google.com/uc?export=view&id=FILE_ID_2",
++	"https://drive.google.com/uc?export=view&id=FILE_ID_3",
++]
