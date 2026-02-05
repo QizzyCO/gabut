1e2c86a9fd7cd047042971a9f1a4e612a4f763e0
QizzyCO <terapptq@gmail.com>
2026-02-05 17:22:24 +0700

    Added firebase.js

--- src/data/galleryUrls.js
+++ src/data/galleryUrls.js
@@ -1,5 +1,8 @@
 export const galleryImageUrls = [
-+	"https://drive.google.com/uc?export=view&id=FILE_ID_1",
-+	"https://drive.google.com/uc?export=view&id=FILE_ID_2",
-+	"https://drive.google.com/uc?export=view&id=FILE_ID_3",
-+]
+  "https://images.pexels.com/photos/3807517/pexels-photo-3807517.jpeg",
+  "https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg",
+  "https://images.pexels.com/photos/3584994/pexels-photo-3584994.jpeg",
+  "https://images.pexels.com/photos/3775857/pexels-photo-3775857.jpeg",
+  "https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg",
+  "https://images.pexels.com/photos/3808014/pexels-photo-3808014.jpeg",
+]
